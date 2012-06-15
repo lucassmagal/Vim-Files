6d95c6165d4a141ae2a12a27711711c9bb4b5655
Lucas Sampaio <magal.lucas@gmail.com>
2012-06-15 13:32:24 -0300

    README and isntall.sh updated

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@
 echo "~~ Installing lucassmagal's vim files ~~"
 
 if [ ! $(which git) ]; then
-    echo "Git não está instalado, processo abortado."
+    echo "Git não está instalado, processo abortado"
     exit 1
 fi
 
 # downloading and configuring files
-# git clone git://github.com/lucassmagal/Vim-Files.git ~/.vim
-#cd ~/.vim
-#git submodule init
-#git submodule update
-#ln ~/.vim/.vimrc ~/.vimrc
-#ln ~/.vim/.gvimrc ~/.gvimrc
+git clone git://github.com/lucassmagal/Vim-Files.git ~/.vim
+cd ~/.vim
+git submodule init
+git submodule update
+ln ~/.vim/.vimrc ~/.vimrc
+ln ~/.vim/.gvimrc ~/.gvimrc
 
-echo "~~ DONE! Now, open 'README.mkd' and learn some commands and shortcuts ~~"
+echo "~~ DONE! Now, open '~/.vim/README.mkd' and learn some commands and shortcuts ~~"
